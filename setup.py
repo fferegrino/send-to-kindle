cbdbc142a458102e5eef27fe997221cbe8b3aef6
Antonio Feregrino <antonio.feregrino@outlook.com>
2019-09-07 07:03:49 +0100

    Bump version: 0.3.0 -> 0.4.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = Path(root_dir, "requirements.txt")
 requirements_dev = Path(root_dir, "requirements-dev.txt")
 packages = find_packages(where=root_dir, exclude=["tests*"])
 
-VERSION = "0.3.0"
+VERSION = "0.4.0"
 
 
 class VerifyVersionCommand(install):
